ba7664adb16d32778699cf7e94f4012fb91fa524
i.kim <i.kim@haulmont.com>
2020-09-21 20:36:31 +0400

    Initial commit

---
+++ app/src/main/java/com/example/metronom/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.metronom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private float speed ;
+    MediaPlayer mp;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mp= MediaPlayer.create(MainActivity.this,R.raw.music);
+    }
+
+    public void play100(View view) {
+        if (mp.isPlaying()){
+            mp.stop();
+        }
+        speed=1f;
+        mp.setPlaybackParams(mp.getPlaybackParams().setSpeed(speed));
+        mp.start();
+    }
+
+    public void play150(View view) {
+        if (mp.isPlaying()){
+            mp.stop();
+        }
+        speed=1.5f;
+        mp.setPlaybackParams(mp.getPlaybackParams().setSpeed(speed));
+        mp.start();
+    }
+
+    public void play200(View view) {
+        if (mp.isPlaying()){
+            mp.stop();
+        }
+        speed=2f;
+        mp.setPlaybackParams(mp.getPlaybackParams().setSpeed(speed));
+        mp.start();
+    }
+
+
+}
